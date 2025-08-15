c4f22224f91211411009a37ecbcc54bf9487a5ba
sbogde <sbogde@gmail.com>
2025-08-15 13:11:03 +0100

    Welcome to u8what.netlify.app!

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -56,7 +56,7 @@ export default function Welcome() {
                     : "primary.light",
               }}
             >
-              u8what!!
+              u8what.netlify.app!
             </Typography>
           </Typography>
         </Stack>
